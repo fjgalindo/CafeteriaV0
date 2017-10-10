e906a42ce76fce8ba408780dbd2d8d8e82361c27
fjgalindo <fjgalindo@iesfuentesanluis.org>
2017-10-10 17:28:13 +0200

    Cabecera actualizada

--- src/js/header.js
+++ src/js/header.js
@@ -3,7 +3,7 @@ window.onload = function() {
 }
 
 function eventos() {
-    var boton_login = document.getElementById("boton_login");
+    var boton_login = document.getElementById("entrar");
     boton_login.addEventListener("click", function(){
         this.preventDefault()
         var cuadro_login = document.getElementById('login');
