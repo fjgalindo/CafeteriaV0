45ea17b847cf07dd2ea04c0b3ecb071c5f5de7fd
fjgalindo <fjgalindo@iesfuentesanluis.org>
2017-10-11 14:06:29 +0200

    commit

--- src/js/header.js
+++ src/js/header.js
@@ -1,21 +1,3 @@
-window.onload = function() {
-    eventos();
-}
-
-function eventos() {
-    var boton_login = document.getElementById("entrar");
-    boton_login.addEventListener("click", function(){
-        this.preventDefault()
-        var cuadro_login = document.getElementById('login');
-        if(cuadro_login.style.display="none"){
-            cuadro_login.style.display="block";
-        } else {
-            cuadro_login.style.display="none";
-        }
-    });
-}
-
-/*
 function loguear(evento){
     evento.preventDefault();
     console.log('1');
@@ -32,11 +14,7 @@ function loguear(evento){
 }
 
 window.onload=function(){
-
     document.getElementById("entrar").addEventListener("click",loguear,false);
 
-
-
-
 }
-*/
+
