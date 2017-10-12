ee1abf72197593d31c2425897d81ebf9c2551aed
fjgalindo <fjgalindo@iesfuentesanluis.org>
2017-10-12 17:26:16 +0200

    Ahora los botones de la cabecera se iluminan al pasar sobre ellos.

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,16 @@
 function logear(evento) {
     evento.preventDefault();
-    console.log('1');
-
     var divlog = document.getElementById("login");
     if (divlog.style.display == "none" || divlog.style.display == "") {
-        console.log('2');
         divlog.style.display = "block";
     } else {
-        console.log('3');
         divlog.style.display = "none";
     }
 }
 
 window.onload = function () {
-    document.getElementById("entrar").addEventListener("click", logear, false);
+    document.getElementById("boton_entrar").addEventListener("click", logear, false);
+    document.onselectstart = function () {
+        return false;
+    }
 }
