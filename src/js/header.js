4a9d7004c6658e8aa39fc3fcd0f14ae24ab88181
fjgalindo <fjgalindo@iesfuentesanluis.org>
2017-10-17 20:32:48 +0200

    Funcion Login

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,26 @@
-function login(evento) {
+window.onload = function () {
+    loadEvents();
+}
+
+function loadEvents() {
+    document.getElementById("link_entrar").addEventListener("click", mostrarlogin, false);
+
+    document.getElementById("entrar").addEventListener("click", entrar, false);
+}
+
+function mostrarlogin(evento) {
     evento.preventDefault();
     var divlog = document.getElementById("login");
     if (divlog.style.display == "none" || divlog.style.display == "") {
         divlog.style.display = "block";
-
     } else {
         divlog.style.display = "none";
     }
 }
 
-window.onload = function () {
-    document.getElementById("boton_entrar").addEventListener("click", login, false);
-    document.onselectstart = function () {
-        return false;
-    }
+function entrar() {
+    alert("Bienvenido");
+
+    var cabecera = document.getElementById("cabecera");
+
 }
