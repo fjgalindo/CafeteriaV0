df63160288b6fb13b4251329e99fda285e3efc89
vdcabedo <vdcabedo@iesfuentesanluis.org>
2017-10-19 21:36:35 +0200

    javascript menu completado

--- src/js/main.js
+++ src/js/main.js
@@ -1,129 +1,133 @@
-window.onload=function(){
+window.onload = function () {
 
     // evento para cambio al menú de los bocadillos
-    document.getElementById("link_bocadillos").addEventListener("click",cambioBocadillos,false);
+    document.getElementById("link_bocadillos").addEventListener("click", cambioBocadillos, false);
 
     // evento para cambio al menú de las bebidas
-    document.getElementById("link_bebidas").addEventListener("click",cambioBebidas,false);
+    document.getElementById("link_bebidas").addEventListener("click", cambioBebidas, false);
 
     // evento para cambio al menú de los cafés
-    document.getElementById("link_cafes").addEventListener("click",cambioCafes,false);
+    document.getElementById("link_cafes").addEventListener("click", cambioCafes, false);
 
     // evento para cambio al menú de los snakcs
-    document.getElementById("link_snacks").addEventListener("click",cambioSnacks,false);
+    document.getElementById("link_snacks").addEventListener("click", cambioSnacks, false);
 
 
-    document.getElementById("producto0").addEventListener("mouseover",mostrarIngredientes,false);
+    var bocadillos = 10;
 
-    document.getElementById("producto0").addEventListener("mouseout",ocultarIngredientes,false);
+    for (var i = 0; i < bocadillos; i++) {
+        document.getElementById("producto_" + i).addEventListener("mouseover", mostrarIngredientes, false);
+    }
+
+
+    for (var j = 0; j < bocadillos; j++) {
+        document.getElementById("producto_" + j).addEventListener("mouseout", ocultarIngredientes, false);
+    }
+
 
 
 }
 
 
 // Función que muestra el menú de los bocadillos
-function cambioBocadillos(){
+function cambioBocadillos() {
 
-    var divBocadillos=document.getElementById("bocadillos");
-    divBocadillos.style.display="block";
+    var divBocadillos = document.getElementById("bocadillos");
+    divBocadillos.style.display = "block";
 
-    var divBebidas=document.getElementById("bebidas");
-    divBebidas.style.display="none";
+    var divBebidas = document.getElementById("bebidas");
+    divBebidas.style.display = "none";
 
-    var divCafes=document.getElementById("cafes");
-    divCafes.style.display="none";
+    var divCafes = document.getElementById("cafes");
+    divCafes.style.display = "none";
 
-    var divSnacks=document.getElementById("snacks");
-    divSnacks.style.display="none";
+    var divSnacks = document.getElementById("snacks");
+    divSnacks.style.display = "none";
 }
 
 // Función que muestra el menú de las bebidas
-function cambioBebidas(){
+function cambioBebidas() {
 
-    var divBocadillos=document.getElementById("bocadillos");
-    divBocadillos.style.display="none";
+    var divBocadillos = document.getElementById("bocadillos");
+    divBocadillos.style.display = "none";
 
-    var divBebidas=document.getElementById("bebidas");
-    divBebidas.style.display="block";
+    var divBebidas = document.getElementById("bebidas");
+    divBebidas.style.display = "block";
 
-    var divCafes=document.getElementById("cafes");
-    divCafes.style.display="none";
+    var divCafes = document.getElementById("cafes");
+    divCafes.style.display = "none";
 
-    var divSnacks=document.getElementById("snacks");
-    divCafes.style.display="none";
+    var divSnacks = document.getElementById("snacks");
+    divCafes.style.display = "none";
 }
 
 // Función que muestra el menú de los cafés
-function cambioCafes(){
+function cambioCafes() {
 
-    var divBocadillos=document.getElementById("bocadillos");
-    divBocadillos.style.display="none";
+    var divBocadillos = document.getElementById("bocadillos");
+    divBocadillos.style.display = "none";
 
-    var divBebidas=document.getElementById("bebidas");
-    divBebidas.style.display="none";
+    var divBebidas = document.getElementById("bebidas");
+    divBebidas.style.display = "none";
 
-    var divCafes=document.getElementById("cafes");
-    divCafes.style.display="block";
+    var divCafes = document.getElementById("cafes");
+    divCafes.style.display = "block";
 
-    var divSnacks=document.getElementById("snacks");
-    divSnacks.style.display="none";
+    var divSnacks = document.getElementById("snacks");
+    divSnacks.style.display = "none";
 }
 
 // Función que muestra el menú de los snacks
-function cambioSnacks(){
+function cambioSnacks() {
 
-    var divBocadillos=document.getElementById("bocadillos");
-    divBocadillos.style.display="none";
+    var divBocadillos = document.getElementById("bocadillos");
+    divBocadillos.style.display = "none";
 
-    var divBebidas=document.getElementById("bebidas");
-    divBebidas.style.display="none";
+    var divBebidas = document.getElementById("bebidas");
+    divBebidas.style.display = "none";
 
-    var divCafes=document.getElementById("cafes");
-    divCafes.style.display="none";
+    var divCafes = document.getElementById("cafes");
+    divCafes.style.display = "none";
 
-    var divSnacks=document.getElementById("snacks");
-    divSnacks.style.display="block";
+    var divSnacks = document.getElementById("snacks");
+    divSnacks.style.display = "block";
 }
 
 
-function mostrarIngredientes(){
+function mostrarIngredientes() {
 
+    var id = this.id;
+    var nombreId = id.split("_");
+    var numeroId = nombreId[1];
 
-    var producto= document.getElementById("producto0");
-    var foto= document.getElementById("foto_bocadillo0");
-    var info= document.getElementById("info_bocadillo0");
+    var producto = document.getElementById("producto_" + numeroId);
+    producto.style.width = "30%";
+    producto.style.height = "60%";
+    producto.style.cursor = "pointer";
 
-    producto.style.width="25%";
-    producto.style.height="55%";
-    producto.style.cursor="pointer";
-    //producto.style.borderBottomLeftRadius="0px";
-    //producto.style.borderBottomRightRadiusRadius="0px";
-    foto.style.display="none";
+    var foto = document.getElementById("foto_bocadillo" + numeroId);
+    foto.style.display = "none";
 
-
-
-
-    var ingrediente= document.getElementById("ingrediente0");
-    ingrediente.style.display="block";
-    ingrediente.style.border="#2c2c2c 2px solid";
-    ingrediente.style.backgroundColor="rgba(255, 255, 255, 0.38);";
-    ingrediente.style.color="white";
-    ingrediente.style.width="auto";
-    ingrediente.style.height="70%";
-}
-
-function ocultarIngredientes(){
-
-
-    var producto= document.getElementById("producto0");
-
-    producto.style.width="20%";
-    producto.style.height="40%";
-
-    var ingrediente= document.getElementById("ingrediente0");
-    ingrediente.style.display="none";
-
-
+    var ingrediente = document.getElementById("ingrediente" + numeroId);
+    ingrediente.style.display = "block";
 
 }
 
+function ocultarIngredientes() {
+
+    var id = this.id;
+    var nombreId = id.split("_");
+    var numeroId = nombreId[1];
+
+    var producto = document.getElementById("producto_" + numeroId);
+
+    producto.style.width = "20%";
+    producto.style.height = "40%";
+
+    var foto = document.getElementById("foto_bocadillo" + numeroId);
+    foto.style.display = "block";
+
+    var ingrediente = document.getElementById("ingrediente" + numeroId);
+    ingrediente.style.display = "none";
+
+}
