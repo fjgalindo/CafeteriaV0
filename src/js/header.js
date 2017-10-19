67ab0d11ee424e895b4999399b15206d8e6965c2
fjgalindo <fjgalindo@iesfuentesanluis.org>
2017-10-19 12:19:07 +0200

    Header usuario logeado

--- src/js/header.js
+++ src/js/header.js
@@ -11,7 +11,7 @@ function loadEvents() {
         evento.preventDefault();
         var sobre_nosotros = document.getElementById("sobre_nosotros");
         //var body = document.getElementsByTagName("body");
-        if (sobre_nosotros.style.display == "none"|| sobre_nosotros.style.display == "") {
+        if (sobre_nosotros.style.display == "none" || sobre_nosotros.style.display == "") {
             sobre_nosotros.style.display = "grid";
             sobre_nosotros.style.animation = "skyfall 0.5s 1 linear";
 
@@ -37,22 +37,5 @@ function mostrarlogin(evento) {
 
 function entrar() {
     alert("Bienvenido");
-
-    var cabecera = document.getElementById("cabecera");
-    cabecera.innerHTML = "<header>"+
-    "<script src="+"./js/header.js"+"></script>"+
-        "<h1>CAFETERIA IESFSL</h1>"+
-            "<nav id="+"navegacion"+">"+
-            "<ul>"+
-                "<li><a href="+"../index.html"+">SOBRE NOSOTROS</a></li>"+
-                "<li><a href="+"./html/menu.html"+" >MENU</a></li>"+
-                "<li><a href="+"./html/sugerencias.html"+">SUGERENCIAS</a></li>"+
-                "<li id="+"user_info"+">"+
-                    "<label id="+"head_username"+">Usuario</label>"+
-                    "<img id="+"head_userimg"+ "src="+"../../img/login/usuario.png"+">"+
-                "</li>"+
-            "</ul>"+
-        "</nav>"+
-"</header>"
-;
+    $("#cabecera").load("./html/header_loged.html");
 }
